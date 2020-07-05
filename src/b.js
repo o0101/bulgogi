12e47fa68882742f81f64830b14b33e76bacb1d2
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2020-07-05 01:12:18 +0000

    Fix bug

--- src/b.js
+++ src/b.js
@@ -20,42 +20,80 @@ export function update(view, state, {useBody: useBody = false} = {}) {
   consistentFocus.next();
 }
 
-function *imperfectlySaveAndRestoreFocus() {
-  while(true) {
-    const active = document.activeElement;
-    let selectionStart,selectionEnd;
-    if ( active ) {
-      ({selectionStart,selectionEnd} = active);
-    }
-
-    yield;
-
-    if ( active ) {
-      const newActive = document.querySelector(imperfectlyGetSelector(active));
-      if ( newActive ) {
-        newActive.focus();
-        try {
-          Object.assign(newActive,{selectionStart,selectionEnd});
-        } catch(e) {}
+// state merging export
+  export function merge(state, newStateFlat) {
+    // Notes
+      // normally if we use Object.assign(state, newState), then
+      // nested properties not specified in newState will be overwritten
+      // so merge does a proper deep merge 
+      // but it does it an easy way by requiring all nested property chains 
+      // are already converted to flat strings
+      // like prop1.prop2.prop3
+    [...Object.entries(newStateFlat)].forEach(([key, value]) => {
+      const path = key.split('.');
+      const lastStep = path.pop();
+      let root = state;
+      for( const step of path ) {
+        if ( ! root[step] ) {
+          // fill in gaps
+          root = {};
+        } else {
+          root = root[step];
+        }
       }
-    }
-
-    yield;
+      root[lastStep] = value;
+    });
   }
-}
 
-function imperfectlyGetSelector(el) {
-  // the first html to our selector does not help specificity
-  return `${
-    el.parentElement && el.parentElement.localName != 'html' ? `${imperfectlyGetSelector(el.parentElement)} > ` : ''  
-  }${
-    el.localName
-  }${
-    el.id ? `#${el.id.replace(/\./g, '\\\\.')}` : ''
-  }${
-    el.classList.length ? `.${[...el.classList].join('.')}` : ''
-  }${
-    el.name ? `[name="${el.name}"]` : ''
-  }`;
-}
+// utility exports
+  export function clone(o) {
+    return JSON.parse(JSON.stringify(o));
+  }
 
+// rendering helpers
+// imperfect focus restoring helpers
+	// we need helpers to keep focus where it was before the last render
+	function *imperfectlySaveAndRestoreFocus() {
+		// we use a generator to track focus because 
+		// generators make it easy to keep track of 
+		// state over a repetitve asynchronous process
+
+		while(true) {
+			const active = document.activeElement;
+			let selectionStart,selectionEnd,value;
+
+			if ( active && (active instanceof HTMLInputElement || active instanceof HTMLTextAreaElement) ) {
+				({selectionStart,selectionEnd,value} = active);
+			}
+
+			yield;
+
+			// we don't want to preserve focus across route changes
+			if ( active ) {
+				const newActive = document.querySelector(imperfectlyGetSelector(active));
+				if ( newActive ) {
+					newActive.focus();
+					try {
+						Object.assign(newActive,{selectionStart,selectionEnd});
+					} catch(e) {}
+				}
+			}
+
+			yield;
+		}
+	}
+
+	function imperfectlyGetSelector(el) {
+		// the first html to our selector does not help specificity
+		return `${
+			el.parentElement && el.parentElement.localName != 'html' ? `${imperfectlyGetSelector(el.parentElement)} > ` : ''  
+		}${
+			el.localName
+		}${
+			el.id ? `#${el.id.replace(/\./g, '\\\\.')}` : ''
+		}${
+			el.classList.length ? `.${[...el.classList].join('.')}` : ''
+		}${
+			el.name ? `[name="${el.name}"]` : ''
+		}`;
+	}
